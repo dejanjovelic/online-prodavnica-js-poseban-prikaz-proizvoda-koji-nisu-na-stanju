86465b22cecfa93480cdc3ad94320ab869eca237
dejan.jovelic <dejan.jovelic@gmail.com>
2025-05-04 20:50:11 +0200

    kreiran css fajl zbog link elementa u html kodu, zavrsen kod u JS fajlu. napomena: u html ubacen meta element zbog latinicnih slova, ubacen element za stilizovanje ispisa u JS fajlu kako bi ispis licio na primer

--- example.js
+++ example.js
@@ -1,4 +1,43 @@
 "use strict";
+
+let th = document.querySelectorAll("th") // ubacen kod kako bi se izmenio ispis na web stranici 
+th[0].textContent = "Br"
+th[2].textContent = "Cena($)"
+th[3].textContent = "Dostupan"
+
+function kreirajRed(artikl){
+    let tr = document.createElement("tr") // 2. kreiranje reda tabele
+
+    let jedinstveniBroj = document.createElement("td")
+    let naziv = document.createElement("td")
+    let cena = document.createElement("td")
+    let dostupnostArtikla = document.createElement("td")
+
+    jedinstveniBroj.textContent = artikl.jedinstveniBroj; // popunjavanje td elemenata
+    naziv.textContent = artikl.naziv;
+    cena.textContent = artikl.cena;
+    dostupnostArtikla.textContent = artikl.dostupnostArtikla ? "DA" : "NE"
+
+    if (!(artikl.dostupnostArtikla)) {  // ubacivanje stil elementa
+        tr.style.backgroundColor = "red"
+    }
+
+    tr.appendChild(jedinstveniBroj)   // povezivanje celija sa redom
+    tr.appendChild(naziv)
+    tr.appendChild(cena)
+    tr.appendChild(dostupnostArtikla)
+
+    return tr
+}
+
+function inicijalizujAtrikle(artikli){
+    let tabela = document.querySelector("#artikli") //1. selektovanje tabele
+    for (let artikl of artikli) {
+        let tr = kreirajRed(artikl)    
+        tabela.appendChild(tr)
+    }
+}
+
 class Artikl {
     constructor(jedinstveniBroj, naziv, cena, dostupnostArtikla) {
         this.jedinstveniBroj = jedinstveniBroj;
@@ -14,8 +53,4 @@ const treciArtikl = new Artikl(3, "Miš", 20, true);
 
 const artikli = [prviArtikl, drugiArtikl, treciArtikl]
 
-let th = document.querySelectorAll("th")
-th[0].textContent = "Br"
-th[2].textContent = "Cena($)"
-th[3].textContent = "Dostupan"
-
+inicijalizujAtrikle(artikli)
